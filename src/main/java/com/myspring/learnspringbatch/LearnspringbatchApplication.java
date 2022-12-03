859ff4832076e3b80e4cb66f5b6add5d8b6b259a
codecoffeecode <harshvardhansahay5@gmail.com>
2022-12-03 16:26:18 +0530

    initial commit

---
+++ src/main/java/com/myspring/learnspringbatch/LearnspringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.myspring.learnspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringbatchApplication.class, args);
+	}
+
+}
