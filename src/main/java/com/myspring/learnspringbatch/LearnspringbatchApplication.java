0bebacbcc133fc959da382b0cd293f7cbd9f662b
codecoffeecode <harshvardhansahay5@gmail.com>
2022-12-03 17:34:30 +0530

    Added exclusion for DataSourceAutoConfiguration and Code Reformat

--- src/main/java/com/myspring/learnspringbatch/LearnspringbatchApplication.java
+++ src/main/java/com/myspring/learnspringbatch/LearnspringbatchApplication.java
@@ -1,13 +1,16 @@
 package com.myspring.learnspringbatch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class LearnSpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBatchApplication.class, args);
+    }
 
 }
